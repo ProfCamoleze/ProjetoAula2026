3b3d99747378101f83c510be42e8a0bdb3458abc
ProfCamoleze <fernando.camoleze@docente.fieb.edu.br>
2026-08-24 07:45:15 -0300

    Aula01

---
+++ Assets/Scripts/Dialogo/GerenciadorDialogo.cs
@@ -0,0 +1,63 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GerenciadorDialogo : MonoBehaviour
+{
+    [Header("Interface")]
+    public GameObject painelDialogo;
+    public TMP_Text txtNome;
+    public TMP_Text txtFala;
+    public Image imgRetrato;
+
+    [Header("Player")]
+    public Player player;
+
+    private DadosDialogo dialogoAtual;
+    private int indiceFala;
+
+    private void Start()
+    {
+        painelDialogo.SetActive(false);
+    }
+
+    public void IniciarDialogo(DadosDialogo dialogo)
+    {
+        dialogoAtual = dialogo;
+        indiceFala = 0;
+
+        player.BloquearMovimento();
+        painelDialogo.SetActive(true);
+
+        MostrarFala();
+    }
+
+    private void MostrarFala()
+    {
+        FalaDialogo fala = dialogoAtual.falas[indiceFala];
+
+        txtNome.text = fala.nomePersonagem;
+        txtFala.text = fala.texto;
+        imgRetrato.sprite = fala.retrato;
+    }
+
+    public void ContinuarDialogo()
+    {
+        indiceFala++;
+
+        if (indiceFala < dialogoAtual.falas.Length)
+        {
+            MostrarFala();
+        }
+        else
+        {
+            FecharDialogo();
+        }
+    }
+
+    public void FecharDialogo()
+    {
+        painelDialogo.SetActive(false);
+        player.LiberarMovimento();
+    }
+}
