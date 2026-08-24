bee59cca8e5d02d8e3a6b1f623180492f1396d1d
FERNANDO CAMOLEZE URCCOVICHE <fernando.camoleze@docente.fieb.edu.br>
2026-08-24 10:13:17 -0300

    ok

--- Assets/Scripts/Dialogo/GerenciadorDialogo.cs
+++ Assets/Scripts/Dialogo/GerenciadorDialogo.cs
@@ -26,7 +26,7 @@ public class GerenciadorDialogo : MonoBehaviour
         dialogoAtual = dialogo;
         indiceFala = 0;
 
-       // player.BloquearMovimento();
+       player.bloquearMovimento();
         painelDialogo.SetActive(true);
 
         MostrarFala();
@@ -58,6 +58,6 @@ public class GerenciadorDialogo : MonoBehaviour
     public void FecharDialogo()
     {
         painelDialogo.SetActive(false);
-       // player.LiberarMovimento();
+       player.liberarMovimento();
     }
 }
