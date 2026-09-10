4d56a8756a19524aeefeb96a7ad51eb8b3a49200
Fernando Camoleze Urccoviche <fernando.camoleze@docente.fieb.edu.br>
2026-09-10 11:19:17 -0300

    ok2

--- Assets/Scripts/GerenciadorControles.cs
+++ Assets/Scripts/GerenciadorControles.cs
@@ -1,54 +1,35 @@
-using System.Runtime.InteropServices;
 using UnityEngine;
+using System.Runtime.InteropServices;
 
 public class GerenciadorControles : MonoBehaviour
 {
-    [Header("Referências")]
-    [SerializeField]
-    private GameObject controlesMobile;
 
-    [Header("Teste no Editor")]
-    [SerializeField]
-    private bool testarControlesMobile = false;
+ public GameObject controlesMobile;
+
+
+ bool testarControlesMobile;
+
 
 #if UNITY_WEBGL && !UNITY_EDITOR
 
-    // Função JavaScript que existe no arquivo
-    // DetectorDispositivo.jslib.
     [DllImport("__Internal")]
     private static extern int EhNavegadorMobile();
 
 #endif
 
+
     private void Start()
     {
-        VerificarDispositivo();
-    }
+        bool mostrarControles = testarControlesMobile;
 
-    private void VerificarDispositivo()
-    {
-        bool navegadorMobile = false;
 
 #if UNITY_WEBGL && !UNITY_EDITOR
 
-        // Na Build Web, perguntamos diretamente
-        // ao navegador se ele é mobile.
-        navegadorMobile = EhNavegadorMobile() == 1;
-
-#elif UNITY_EDITOR
-
-        // Dentro do Editor usamos esta opção
-        // apenas para facilitar os testes.
-        navegadorMobile = testarControlesMobile;
-
-#else
-
-        // Caso futuramente seja criada uma Build
-        // Android ou iOS nativa.
-        navegadorMobile = Application.isMobilePlatform;
+        mostrarControles = EhNavegadorMobile() == 1;
 
 #endif
 
-        controlesMobile.SetActive(navegadorMobile);
+
+        controlesMobile.SetActive(mostrarControles);
     }
 }
